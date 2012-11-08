b6afc76c028a62ae3c31cf0321551a77aac1ff88
Zoltan Dezso <dezso.zoltan@gmail.com>
2012-11-08 22:17:29 +0900

    Shorten BRACKETS

--- brackets.rb
+++ brackets.rb
@@ -1,6 +1,3 @@
-$a=''
-%w(+ - * / %).map{|x|$a<<"def#{x}(x);\"(\#{self}#{x}\#{x})\";end;"}
-String.class_eval $a+"def s;self;end"
-$a<<"def s;\"(\#{self})\";end"
-[Fixnum,Bignum].map{|x|x.class_eval $a}
-1.upto(gets.to_i){puts eval(gets).s}
+a=%w(+ - * / %).map{|x|%[def#{x}(x);"(\#{self}#{x}\#{x})";end;]}.join
+[Fixnum,Bignum,String].map{|x|x.class_eval(a+'def s;self.is_a?(String)?self:"(#{self})";end')}
+gets;$<.lines{|x|puts eval(x).s}
